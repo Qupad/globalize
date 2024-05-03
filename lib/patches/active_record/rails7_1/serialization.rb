314e5616869a367de9e2b91152d0dc34a456df6b
Qupad <capzed@gmail.com>
2024-05-03 12:08:57 +0300

    Update serialization.rb

--- lib/patches/active_record/rails7_1/serialization.rb
+++ lib/patches/active_record/rails7_1/serialization.rb
@@ -1,16 +1,28 @@
+# frozen_string_literal: true
+
+# Fixes deprecation and issue with:
+#
+## DEPRECATION WARNING: Passing the class as positional argument is deprecated and will be removed in Rails 7.2.
+## Please pass the class as a keyword argument
+#
+# by dynamically providing type: and coder: if needed
+#
+# Globalize patch to remove warning in rails 7.1 about not providing :type in serialize
 module Globalize
   module AttributeMethods
     module Serialization
+      # Provides type by default depending on :coder and build serializer column
       def serialize(attr_name, class_name_or_coder = Object, **options)
-        if options[:coder].blank?
-          if class_name_or_coder == ::JSON || [:load, :dump].all? { |x| class_name_or_coder.respond_to?(x) }
-            options = options.merge(coder: class_name_or_coder, type: Object)
-          else
-            options = options.merge(coder: default_column_serializer, type: class_name_or_coder)
-          end
-        end
-
         super(attr_name, **options)
+
+        options = if class_name_or_coder == ::JSON
+                    options.merge(coder: JSON, type: class_name_or_coder)
+                  elsif %i[load dump].all? { |x| class_name_or_coder.respond_to?(x) }
+                    options.merge(coder: class_name_or_coder, type: class_name_or_coder)
+                  else
+                    options.merge(coder: YAML, type: class_name_or_coder)
+                  end
+
 
         coder = build_column_serializer(attr_name, options[:coder], options[:type], options[:yaml])
 
